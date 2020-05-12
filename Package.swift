c05ff08621af9fd8723bce4cb631d120cbae1b06
Consuelita <mafalda@Mafaldas-MacBook-Pro.local>
2020-05-12 13:02:33 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/OperatorFoundation/Monolith.git", from: "1.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "1.0.5"),
+        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "2.0.1"),
         .package(url: "https://github.com/OperatorFoundation/Transport.git", from: "0.1.1"),
         .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git", from: "0.0.3")
     ],
