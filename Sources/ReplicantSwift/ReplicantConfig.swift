9a891e3d5830de1538b4ac25bfdf5316a5cfd94a
consuelita <adelita@operatorfoundation.org>
2021-02-22 18:21:46 -0600

    Update ReplicantConfig.swift

--- Sources/ReplicantSwift/ReplicantConfig.swift
+++ Sources/ReplicantSwift/ReplicantConfig.swift
@@ -42,59 +42,59 @@ public struct ReplicantConfig<PolishClientConfigType>: Codable where PolishClien
         guard let data = try? Data(contentsOf: url) else {return nil}
         self.init(from: data)
     }
-//
-//    /// Creates and returns a JSON representation of the ReplicantConfig struct.
-//    public func createJSON() -> Data?
-//    {
-//        let encoder = JSONEncoder()
-//        encoder.outputFormatting = .prettyPrinted
-//
-//        do
-//        {
-//            let configData = try encoder.encode(self)
-//            return configData
-//        }
-//        catch (let error)
-//        {
-//            print("Failed to encode config into JSON format: \(error)")
-//            return nil
-//        }
-//    }
-//
-//    /// Checks for a valid JSON at the provided path and attempts to decode it into a Replicant client configuration file. Returns a ReplicantConfig struct if it is successful
-//    /// - Parameters:
-//    ///     - path: The complete path where the config file is located.
-//    /// - Returns: The ReplicantConfig struct that was decoded from the JSON file located at the provided path, or nil if the file was invalid or missing.
-//    public static func parseJSON(atPath path: String) -> ReplicantConfig?
-//    {
-//        let fileManager = FileManager()
-//
-//
-//        guard let jsonData = fileManager.contents(atPath: path)
-//            else
-//        {
-//            print("\nUnable to get JSON data at pathe: \(path)\n")
-//            return nil
-//        }
-//
-//        return parse(jsonData: jsonData)
-//    }
-//
-//    public static func parse(jsonData: Data) -> ReplicantConfig?
-//    {
-//        let decoder = JSONDecoder()
-//
-//        do
-//        {
-//            let config = try decoder.decode(ReplicantConfig.self, from: jsonData)
-//            return config
-//        }
-//        catch (let error)
-//        {
-//            print("\nUnable to decode JSON into ReplicantConfig: \(error)\n")
-//            return nil
-//        }
-//    }
+
+    /// Creates and returns a JSON representation of the ReplicantConfig struct.
+    public func createJSON() -> Data?
+    {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+
+        do
+        {
+            let configData = try encoder.encode(self)
+            return configData
+        }
+        catch (let error)
+        {
+            print("Failed to encode config into JSON format: \(error)")
+            return nil
+        }
+    }
+
+    /// Checks for a valid JSON at the provided path and attempts to decode it into a Replicant client configuration file. Returns a ReplicantConfig struct if it is successful
+    /// - Parameters:
+    ///     - path: The complete path where the config file is located.
+    /// - Returns: The ReplicantConfig struct that was decoded from the JSON file located at the provided path, or nil if the file was invalid or missing.
+    public static func parseJSON(atPath path: String) -> ReplicantConfig?
+    {
+        let fileManager = FileManager()
+
+
+        guard let jsonData = fileManager.contents(atPath: path)
+            else
+        {
+            print("\nUnable to get JSON data at path: \(path)\n")
+            return nil
+        }
+
+        return parse(jsonData: jsonData)
+    }
+
+    public static func parse(jsonData: Data) -> ReplicantConfig?
+    {
+        let decoder = JSONDecoder()
+
+        do
+        {
+            let config = try decoder.decode(ReplicantConfig.self, from: jsonData)
+            return config
+        }
+        catch (let error)
+        {
+            print("\nUnable to decode JSON into ReplicantConfig: \(error)\n")
+            return nil
+        }
+    }
 }
 
 
