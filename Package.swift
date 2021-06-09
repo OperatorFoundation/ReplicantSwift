77a2aa5bdd1dd403c907dc0ad4985c2afb8aa5f0
consuelita <adelita@operatorfoundation.org>
2021-06-09 12:57:28 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@
 
 import PackageDescription
 
+#if os(iOS) || os(macOS) || os(watchOS) || os(tvOS)
 let package = Package(
     name: "ReplicantSwift",
     platforms: [
@@ -15,28 +16,28 @@ let package = Package(
             targets: ["ReplicantSwift"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.1.2"),
-        .package(url: "https://github.com/OperatorFoundation/Keychain.git", from: "0.1.2"),
-        .package(url: "https://github.com/OperatorFoundation/KeychainLinux.git", from: "0.1.1"),
-        .package(url: "https://github.com/OperatorFoundation/Song.git", from: "0.1.1"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
-        .package(url: "https://github.com/OperatorFoundation/Monolith.git", from: "1.0.2"),
-        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.0.4"),
-        .package(url: "https://github.com/OperatorFoundation/Transport.git", from: "2.3.5"),
-        .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git", from: "0.1.0")
+        .package(url: "https://github.com/OperatorFoundation/Keychain.git",
+                 from: "0.1.2"),
+        .package(url: "https://github.com/OperatorFoundation/Song.git",
+                 from: "0.1.1"),
+        .package(url: "https://github.com/apple/swift-log.git",
+                 from: "1.4.0"),
+        .package(url: "https://github.com/OperatorFoundation/Monolith.git",
+                 from: "1.0.2"),
+        .package(url: "https://github.com/OperatorFoundation/Datable.git",
+                 from: "3.0.4"),
+        .package(url: "https://github.com/OperatorFoundation/Transport.git",
+                 from: "2.3.5"),
+        .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git",
+                 from: "0.1.0")
 ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ReplicantSwift",
             dependencies: [
                 "Monolith", "Datable", "Transport", "Song", "SwiftQueue",
                 .product(name: "Logging", package: "swift-log"),
-                .product(name: "Keychain", package: "Keychain", condition: .when(platforms: [.iOS, .macOS, .watchOS, .tvOS])),
-                .product(name: "KeychainLinux", package: "KeychainLinux", condition: .when(platforms: [.linux])),
-                .product(name: "Crypto", package: "swift-crypto", condition: .when(platforms: [.linux]))
+                .product(name: "Keychain", package: "Keychain"),
             ]
         ),
         .testTarget(
@@ -45,3 +46,46 @@ let package = Package(
     ],
     swiftLanguageVersions: [.v5]
 )
+#else
+let package = Package(
+    name: "ReplicantSwift",
+    products: [
+        .library(
+            name: "ReplicantSwift",
+            targets: ["ReplicantSwift"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-crypto.git",
+                 from: "1.1.2"),
+        .package(url: "https://github.com/OperatorFoundation/KeychainLinux.git",
+                 from: "0.1.1"),
+        .package(url: "https://github.com/OperatorFoundation/Song.git",
+                 from: "0.1.1"),
+        .package(url: "https://github.com/apple/swift-log.git",
+                 from: "1.4.0"),
+        .package(url: "https://github.com/OperatorFoundation/Monolith.git",
+                 from: "1.0.2"),
+        .package(url: "https://github.com/OperatorFoundation/Datable.git",
+                 from: "3.0.4"),
+        .package(url: "https://github.com/OperatorFoundation/Transport.git",
+                 from: "2.3.5"),
+        .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git",
+                 from: "0.1.0")
+],
+    targets: [
+        .target(
+            name: "ReplicantSwift",
+            dependencies: [
+                "Monolith", "Datable", "Transport", "Song", "SwiftQueue",
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "KeychainLinux", package: "KeychainLinux"),
+                .product(name: "Crypto", package: "swift-crypto")
+            ]
+        ),
+        .testTarget(
+            name: "ReplicantSwiftTests",
+            dependencies: ["ReplicantSwift"]),
+    ],
+    swiftLanguageVersions: [.v5]
+)
+#endif
