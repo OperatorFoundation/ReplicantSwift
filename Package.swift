1470458129b00f58708eb60a7d877166bb8f7eb4
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2021-11-05 14:58:36 -0500

    Updated to new Transmission and removed #if os in Package.swift

--- Package.swift
+++ Package.swift
@@ -3,12 +3,13 @@
 
 import PackageDescription
 
-#if os(iOS) || os(macOS) || os(watchOS) || os(tvOS)
 let package = Package(
     name: "ReplicantSwift",
     platforms: [.macOS(.v10_15)],
     products: [.library(name: "ReplicantSwift", targets: ["ReplicantSwift"])],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-crypto.git",
+                 from: "1.1.2"),
         .package(url: "https://github.com/OperatorFoundation/Keychain.git",
                  from: "0.1.2"),
         .package(url: "https://github.com/OperatorFoundation/Song.git",
@@ -19,7 +20,7 @@ let package = Package(
                  from: "1.0.3"),
         .package(url: "https://github.com/OperatorFoundation/Datable.git",
                  from: "3.1.1"),
-        .package(url: "https://github.com/OperatorFoundation/Transmission.git", from: "0.6.0"),
+        .package(url: "https://github.com/OperatorFoundation/Transmission.git", from: "1.0.0"),
         .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git",
                  from: "0.1.1")
     ],
@@ -32,6 +33,7 @@ let package = Package(
                 "Song",
                 "SwiftQueue",
                 "Transmission",
+                .product(name: "Crypto", package: "swift-crypto"),
                 .product(name: "Logging", package: "swift-log"),
                 .product(name: "Keychain", package: "Keychain"),
             ]
@@ -42,43 +44,3 @@ let package = Package(
     ],
     swiftLanguageVersions: [.v5]
 )
-#elseif os(Linux)
-let package = Package(
-    name: "ReplicantSwift",
-    products: [.library(name: "ReplicantSwift", targets: ["ReplicantSwift"])],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-crypto.git",
-                 from: "1.1.2"),
-        .package(url: "https://github.com/OperatorFoundation/KeychainLinux.git",
-                 from: "0.1.1"),
-        .package(url: "https://github.com/OperatorFoundation/Song.git",
-                 from: "0.2.1"),
-        .package(url: "https://github.com/apple/swift-log.git",
-                 from: "1.4.2"),
-        .package(url: "https://github.com/OperatorFoundation/Monolith.git",
-                 from: "1.0.2"),
-        .package(url: "https://github.com/OperatorFoundation/Datable.git",
-                 from: "3.0.6"),
-        .package(url: "https://github.com/OperatorFoundation/Transmission.git", from: "0.6.0"),
-        .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git",
-                 from: "0.1.0")
-    ],
-    targets: [
-        .target(
-            name: "ReplicantSwift",
-            dependencies: [
-                "Datable",
-                "Monolith",
-                "Song",
-                "Transmission",
-                "SwiftQueue",
-                .product(name: "Logging", package: "swift-log"),
-                .product(name: "KeychainLinux", package: "KeychainLinux"),
-                .product(name: "Crypto", package: "swift-crypto")
-            ]),
-        .testTarget(
-            name: "ReplicantSwiftTests",
-            dependencies: ["ReplicantSwift"]),
-    ],
-    swiftLanguageVersions: [.v5])
-#endif
