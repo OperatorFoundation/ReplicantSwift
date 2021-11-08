b21e8fc0f5d484edc9901a1a2efb2709f6681dc0
CryptoSax <joshua@operatorfoundation.org>
2021-11-08 14:45:24 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .package(url: "https://github.com/OperatorFoundation/Keychain.git",
                  from: "1.0.0"),
         .package(url: "https://github.com/OperatorFoundation/Song.git",
-                 from: "0.1.7"),
+                 from: "0.2.2"),
         .package(url: "https://github.com/apple/swift-log.git",
                  from: "1.4.2"),
         .package(url: "https://github.com/OperatorFoundation/Monolith.git",
