240add6981b294cab36c7d7140783f64291cdf77
CryptoSax <joshua@operatorfoundation.org>
2021-11-11 06:55:15 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,16 +14,16 @@ let package = Package(
         .package(url: "https://github.com/OperatorFoundation/Keychain.git",
                  from: "1.0.0"),
         .package(url: "https://github.com/OperatorFoundation/Song.git",
-                 from: "0.2.2"),
+                 from: "0.2.3"),
         .package(url: "https://github.com/apple/swift-log.git",
                  from: "1.4.2"),
         .package(url: "https://github.com/OperatorFoundation/Monolith.git",
                  from: "1.0.4"),
         .package(url: "https://github.com/OperatorFoundation/Datable.git",
-                 from: "3.1.1"),
-        .package(url: "https://github.com/OperatorFoundation/Transmission.git", from: "1.0.1"),
+                 from: "3.1.2"),
+        .package(url: "https://github.com/OperatorFoundation/Transmission.git", from: "1.0.4"),
         .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git",
-                 from: "0.1.1")
+                 from: "0.1.2")
     ],
     targets: [
         .target(
