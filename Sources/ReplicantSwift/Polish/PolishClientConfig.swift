5470f2ffcb9bbdf91b623e2b279c3ae1bb3fe00b
CryptoSax <joshua@operatorfoundation.org>
2022-11-29 23:10:58 -0600

    fixed host:port split

--- Sources/ReplicantSwift/Polish/PolishClientConfig.swift
+++ Sources/ReplicantSwift/Polish/PolishClientConfig.swift
@@ -29,15 +29,17 @@ extension PolishClientConfig: PolishConfig
     public func polish(_ connection: TransmissionTypes.Connection, _ logger: Logger) throws -> TransmissionTypes.Connection
     {
         let addressArray = self.serverAddress.split(separator: ":")
-        let host = addressArray[0].base
-        let port = addressArray[1].base
-        let portUint = port.uint16
+        let host = String(addressArray[0])
+        guard let port = UInt16(addressArray[1]) else {
+            throw PolishClientConfigError.invalidPort
+        }
+        
         guard let ipv4 = IPv4Address(host) else
         {
             throw PolishClientConfigError.notV4Address(host)
         }
 
-        let endpoint = NWEndpoint.hostPort(host: NWEndpoint.Host.ipv4(ipv4), port: NWEndpoint.Port(integerLiteral: portUint))
+        let endpoint = NWEndpoint.hostPort(host: NWEndpoint.Host.ipv4(ipv4), port: NWEndpoint.Port(integerLiteral: port))
         let config = ShadowConfig.ShadowClientConfig(serverAddress: self.serverAddress, serverPublicKey: self.serverAddress, mode: .DARKSTAR, transport: "shadow")
         guard let result = DarkStarClientConnection(connection: connection, endpoint: endpoint, parameters: .tcp, config: config, logger: logger) else
         {
@@ -57,6 +59,7 @@ extension PolishClientConfig: PolishConfig
 public enum PolishClientConfigError: Error
 {
     case nullDarkStarConnection
+    case invalidPort
     case notV4Address(String)
     case transportToTransmissionFailed
 }
--- Sources/ReplicantSwift/Polish/PolishServerConfig.swift
+++ Sources/ReplicantSwift/Polish/PolishServerConfig.swift
@@ -29,15 +29,16 @@ extension PolishServerConfig: PolishConfig
     public func polish(_ connection: TransmissionTypes.Connection, _ logger: Logger) throws -> TransmissionTypes.Connection
     {
         let addressArray = self.serverAddress.split(separator: ":")
-        let host = addressArray[0].base
-        let port = addressArray[1].base
-        let portUint = port.uint16
+        let host = String(addressArray[0])
+        guard let port = UInt16(addressArray[1]) else {
+            throw PolishServerConfigError.invalidPort
+        }
         guard let ipv4 = IPv4Address(host) else
         {
             throw PolishServerConfigError.notV4Address(host)
         }
 
-        let endpoint = NWEndpoint.hostPort(host: NWEndpoint.Host.ipv4(ipv4), port: NWEndpoint.Port(integerLiteral: portUint))
+        let endpoint = NWEndpoint.hostPort(host: NWEndpoint.Host.ipv4(ipv4), port: NWEndpoint.Port(integerLiteral: port))
         let config = ShadowConfig.ShadowServerConfig(serverAddress: self.serverAddress, serverPrivateKey: self.serverPrivateKey, mode: .DARKSTAR, transport: "shadow")
         guard let result = DarkStarServerConnection(connection: connection, endpoint: endpoint, parameters: .tcp, config: config, logger: logger) else
         {
@@ -55,6 +56,7 @@ extension PolishServerConfig: PolishConfig
 
 public enum PolishServerConfigError: Error
 {
+    case invalidPort
     case nullDarkStarConnection
     case notV4Address(String)
     case transportToTransmissionFailed
