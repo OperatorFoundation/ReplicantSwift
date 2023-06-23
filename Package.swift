eb171dc09d0f7f10e834c041a381c6cdab1b36c1
Jessica9-star <68959096+Jessica9-star@users.noreply.github.com>
2023-06-22 22:58:41 -0500

    Updated package to .macOS(.v13)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "ReplicantSwift",
     platforms: [
         .iOS(.v15),
-        .macOS(.v12),
+        .macOS(.v13),
     ],
     products: [.library(name: "ReplicantSwift", targets: ["ReplicantSwift"])],
     dependencies: [
