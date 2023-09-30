78effc37909ef5968ae3a1c579a4a606cab1831e
bragelbytes <joebragel@gmail.com>
2023-09-30 11:24:57 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     products: [.library(name: "ReplicantSwift", targets: ["ReplicantSwift"])],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "2.5.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"),
 
         .package(url: "https://github.com/OperatorFoundation/Datable", branch: "main"),
