6555097c8d113050e6b497d1daec09c2cd0f4395
consuelita <adelita@operatorfoundation.org>
2024-03-19 14:34:55 -0500

    Update Starburst handleSMTPClient

--- Sources/ReplicantSwift/ToneBurst/Starburst/Starburst.swift
+++ Sources/ReplicantSwift/ToneBurst/Starburst/Starburst.swift
@@ -58,60 +58,8 @@ public struct StarburstInstanceAsync
             try await handleSMTPServer()
         }
     }
-
-    func handleSMTPClient() async throws
-    {
-        guard let firstClientListen = ListenTemplate(Template("220 $1 SMTP service ready\r\n"), patterns: [ExtractionPattern("^([a-zA-Z0-9.-]+)", .string)], maxSize: 253, maxTimeoutSeconds: Int.max) else {
-            throw StarburstError.listenFailed
-        }
-        
-        let _ = try await listen(template: firstClientListen)
-
-        try await speak(template: Template("EHLO $1\r\n"), details: [Detail.string("mail.imc.org")])
-
-        guard let secondClientListen = ListenTemplate(Template("$1\r\n"), patterns: [ExtractionPattern("250 (STARTTLS)", .string)], maxSize: 253, maxTimeoutSeconds: 10) else {
-            throw StarburstError.listenFailed
-        }
-        
-        _ = try await listen(template: secondClientListen)
-
-        try await speak(string: "STARTTLS\r\n")
-
-        guard let thirdClientListen = ListenTemplate(Template("$1\r\n"), patterns: [ExtractionPattern("^(.+)\r\n", .string)], maxSize: 253, maxTimeoutSeconds: 10) else {
-            throw StarburstError.listenFailed
-        }
-        
-        _ = try await listen(template: thirdClientListen)
-    }
     
-    func handleSMTPClient2() async throws
-    {
-//        guard let firstClientListen = ListenTemplate(Template("220 $1 SMTP service ready\r\n"), patterns: [ExtractionPattern("^([a-zA-Z0-9.-]+)", .string)], maxSize: 253, maxTimeoutSeconds: Int.max) else {
-//            throw StarburstError.listenFailed
-//        }
-        
-        let _ = try await listen(structuredText: StructuredText(TypedText.text("220 "), TypedText.regexp("^([a-zA-Z0-9.-]+)"), TypedText.text(" SMTP service ready"), TypedText.newline(.crlf)), maxSize: 253, timeout: .seconds(Int.max))
-
-        // try await speak(template: Template("EHLO $1\r\n"), details: [Detail.string("mail.imc.org")])
-        try await speak(structuredText: StructuredText(TypedText.text("EHLO mail.imc.org"), TypedText.newline(.crlf)))
-
-//        guard let secondClientListen = ListenTemplate(Template("$1\r\n"), patterns: [ExtractionPattern("250 (STARTTLS)", .string)], maxSize: 253, maxTimeoutSeconds: 10) else {
-//            throw StarburstError.listenFailed
-//        }
-        
-        _ = try await listen(structuredText: StructuredText(TypedText.text("250 STARTTLS"), TypedText.newline(.crlf)), maxSize: 253, timeout: .seconds(10))
-
-        // try await speak(string: "STARTTLS\r\n")
-        try await speak(structuredText: StructuredText(TypedText.text("STARTTLS"), TypedText.newline(.crlf)))
-
-//        guard let thirdClientListen = ListenTemplate(Template("$1\r\n"), patterns: [ExtractionPattern("^(.+)\r\n", .string)], maxSize: 253, maxTimeoutSeconds: 10) else {
-//            throw StarburstError.listenFailed
-//        }
-        
-        _ = try await listen(structuredText: StructuredText(TypedText.regexp("^(.+)$"), TypedText.newline(.crlf)), maxSize: 253, timeout: .seconds(10))
-    }
-    
-    private func handleSMTPClient3() async throws
+    private func handleSMTPClient() async throws
     {
         let _ = try await self.listen(structuredText: StructuredText(TypedText.text("220 "), TypedText.regexp("^([a-zA-Z0-9.-]+)"), TypedText.text(" SMTP service ready"), TypedText.newline(Newline.crlf)), maxSize: 253, timeout: Duration.seconds(9223372036854775807))
         try await self.speak(structuredText: StructuredText(TypedText.text("EHLO mail.imc.org"), TypedText.newline(Newline.crlf)))
@@ -122,65 +70,41 @@ public struct StarburstInstanceAsync
 
     func handleSMTPServer() async throws
     {
-        try await speak(template: Template("220 $1 SMTP service ready\r\n"), details: [Detail.string("mail.imc.org")])
-        
-        guard let firstServerListen = ListenTemplate(Template("EHLO $1\r\n"), patterns: [ExtractionPattern("^([a-zA-Z0-9.-]+)\r", .string)], maxSize: 253, maxTimeoutSeconds: 10) else {
-            throw StarburstError.listenFailed
-        }
-        
-        _ = try await listen(template: firstServerListen)
-
-        // % 5 is mod, which divides by five, discards the result, then returns the remainder
-        let hour = Calendar.current.component(.hour, from: Date()) % 5
-        let welcome: String
-        switch hour
-        {
-            // These are all real SMTP welcome messages found in online examples of SMTP conversations.
-            case 0:
-                welcome = "offers a warm hug of welcome"
-            case 1:
-                welcome = "is my domain name."
-            case 2:
-                welcome = "I am glad to meet you"
-            case 3:
-                welcome = "says hello"
-            case 4:
-                welcome = "Hello"
-
-            default:
-                welcome = ""
-        }
-
-        try await speak(template: Template("250-$1 $2\r\n250-$3\r\n250-$4\r\n250 $5\r\n"), details: [Detail.string("mail.imc.org"), Detail.string(welcome), Detail.string("8BITMIME"), Detail.string("DSN"), Detail.string("STARTTLS")])
-
-        // FIXME: not sure about this size
-        let _: String = try await listen(size: "STARTTLS\r\n".count + 1) // \r\n is counted as one on .count
-
-        try await speak(template: Template("220 $1\r\n"), details: [Detail.string("Go ahead")])
-    }
-
-    func speak(data: Data) async throws
-    {
-        try await connection.write(data)
-    }
-
-    func speak(string: String) async throws
-    {
-        try await connection.writeString(string: string)
-    }
-
-    func speak(template: Template, details: [Detail]) async throws
-    {
-        do
-        {
-            let string = try Ghostwriter.generate(template, details)
-            try await connection.writeString(string: string)
-        }
-        catch
-        {
-            print(error)
-            throw StarburstError.writeFailed
-        }
+//        try await speak(template: Template("220 $1 SMTP service ready\r\n"), details: [Detail.string("mail.imc.org")])
+//        
+//        guard let firstServerListen = ListenTemplate(Template("EHLO $1\r\n"), patterns: [ExtractionPattern("^([a-zA-Z0-9.-]+)\r", .string)], maxSize: 253, maxTimeoutSeconds: 10) else {
+//            throw StarburstError.listenFailed
+//        }
+//        
+//        _ = try await listen(template: firstServerListen)
+//
+//        // % 5 is mod, which divides by five, discards the result, then returns the remainder
+//        let hour = Calendar.current.component(.hour, from: Date()) % 5
+//        let welcome: String
+//        switch hour
+//        {
+//            // These are all real SMTP welcome messages found in online examples of SMTP conversations.
+//            case 0:
+//                welcome = "offers a warm hug of welcome"
+//            case 1:
+//                welcome = "is my domain name."
+//            case 2:
+//                welcome = "I am glad to meet you"
+//            case 3:
+//                welcome = "says hello"
+//            case 4:
+//                welcome = "Hello"
+//
+//            default:
+//                welcome = ""
+//        }
+//
+//        try await speak(template: Template("250-$1 $2\r\n250-$3\r\n250-$4\r\n250 $5\r\n"), details: [Detail.string("mail.imc.org"), Detail.string(welcome), Detail.string("8BITMIME"), Detail.string("DSN"), Detail.string("STARTTLS")])
+//
+//        // FIXME: not sure about this size
+//        let _: String = try await listen(size: "STARTTLS\r\n".count + 1) // \r\n is counted as one on .count
+//
+//        try await speak(template: Template("220 $1\r\n"), details: [Detail.string("Go ahead")])
     }
     
     func speak(structuredText: StructuredText) async throws
@@ -194,65 +118,6 @@ public struct StarburstInstanceAsync
         {
             print(error)
             throw StarburstError.writeFailed
-        }
-    }
-
-    func listen(size: Int) async throws -> Data
-    {
-        return try await connection.readSize(size)
-    }
-
-    func listen(size: Int) async throws -> String
-    {
-        let data = try await connection.readSize(size)
-        return data.string
-    }
-
-    func listen(template: ListenTemplate) async throws -> [Detail]
-    {
-        let listenTask: Task<[Detail]?, Error> = Task {
-            var buffer = Data()
-            while buffer.count < template.maxSize
-            {
-                do {
-                    let byte = try await connection.readSize(1)
-                    
-                    buffer.append(byte)
-                    
-                    guard let string = String(data: buffer, encoding: .utf8) else
-                    {
-                        // This could fail because we're in the middle of a UTF8 rune.
-                        continue
-                    }
-                    
-                    do
-                    {
-                        return try Ghostwriter.parse(template.template, template.patterns, string)
-                    }
-                    catch
-                    {
-                        continue
-                    }
-                } catch {
-                    return nil
-                }
-            }
-            
-            return nil
-        }
-        
-        let _ = Task {
-            try await Task.sleep(for: .seconds(60))
-            listenTask.cancel()
-        }
-        
-        do {
-            guard let result = try await listenTask.value else {
-                throw StarburstError.readFailed
-            }
-            return result
-        } catch {
-            throw StarburstError.timeout
         }
     }
     
@@ -339,6 +204,13 @@ public struct StarburstInstanceAsync
         _ = lock.wait(timeout: .now() + seconds)
     }
 }
+
+public enum StarburstMode: String, Codable 
+{
+    case SMTPServer
+    case SMTPClient
+}
+
 public enum StarburstError: Error
 {
     case timeout
